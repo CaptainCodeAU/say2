d32ca434a8180432dd00bbd803f9e7c79a86ba71
CaptainCodeAU <captaincodeau@gmail.com>
2026-09-08 17:03:19 +1000

    Release v1.2.1
    C-Sess-Id: d9c89b08-1369-4938-81c8-e3dffbc8873d
    C-Web-Id:
    C-Branch: main
    C-Worktree: say2
    C-Wt-Path: ~/CODE/CaptainCodeAU/say2

--- Formula/say2.rb
+++ Formula/say2.rb
@@ -1,7 +1,7 @@
 class Say2 < Formula
   desc "Render installed neural Siri voices from the macOS command-line"
   homepage "https://github.com/CaptainCodeAU/say2"
-  url "https://github.com/CaptainCodeAU/say2.git", tag: "v1.2.0"
+  url "https://github.com/CaptainCodeAU/say2.git", tag: "v1.2.1"
   license "MIT"
   head "https://github.com/CaptainCodeAU/say2.git", branch: "main"
 
--- Sources/Say2Core/Models.swift
+++ Sources/Say2Core/Models.swift
@@ -1,7 +1,7 @@
 import CoreAudioTypes
 import Foundation
 
-public let say2Version = "1.2.0"
+public let say2Version = "1.2.1"
 public let say2SchemaVersion = 1
 
 public enum ExitCode: Int32, Sendable {
